8c32763194ccf5dc6b607578f8433f0b5b59a219
jeevan-gaikwad <jcgaikwad@gmail.com>
2018-12-26 17:18:35 +0530

    array return ref corrected

--- basics/func_ret_lval_demo.cpp
+++ basics/func_ret_lval_demo.cpp
@@ -14,9 +14,13 @@ auto update(std::string[] &arr){
 	return arr;
 }
 */
+typedef int arr_i_t[2];
 
-int[] &update2(int[] &arr) {
+arr_i_t &update2() {
 
+	static arr_i_t arr;
+	arr[0]=22;
+	arr[1]=23;
 	return arr;
 }
 
@@ -30,8 +34,9 @@ int main(void) {
 
 	//update(str_arr);
 	//std::cout<<str_arr[0]<<std::endl;
-	int arr[3]={9,8,7};
-	update2(arr);
-	std::cout<<" ";
+	int arr[2]={8,7};
+	int* sample_arr = update2();
+
+	std::cout<<sample_arr[0]<<" "<<sample_arr[1]<<std::endl;
 	return 0;
 }
